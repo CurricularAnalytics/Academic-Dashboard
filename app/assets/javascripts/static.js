f458533e4f3a8bddbbcf88c44a297064666b33be
Kevin Warne <kjw988@gmail.com>
2014-11-10 15:07:21 -0700

    added sign-in modal

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,31 +1,22 @@
 var header_height = 80;
 
 $(document).ready(function(){ 
-	var sign_in_form = $('#sign-in-form'),
-			sign_in_link = $('#sign-in-link');
-			
-	var sign_in_form_width = sign_in_form.width(),
-		 	sign_in_link_width = sign_in_link.width();
-	
-	sign_in_form.css('width','0px');
-	
-	sign_in_link.click(function(){
-	  sign_in_form.animate({"width":sign_in_form_width+'px',"opacity":'1'}, 500).find('#user_email').focus(); 
-	  sign_in_link.animate({"opacity":'0','width':'0px'}, 500)
-		$(document).on('click.hide_sign_in_form',function(e){
-	    if (!sign_in_link.is(e.target)
-					&& !sign_in_form.is(e.target) 
-	        && sign_in_form.has(e.target).length === 0)
-	    {
-				$(document).unbind('click.hide_sign_in_form')
-			  sign_in_form.animate({"width":'0px',"opacity":'0'}, 500);
-			  sign_in_link.animate({"opacity":'1','width':sign_in_link_width+'px'}, 500)
-	    }
-		})
+	var	sign_in_link = $('.sign-in-link');
+
+	sign_in_link.click( function() {
+		$('body').css('overflow', 'hidden');
+		$('#screen').show();
+		$('#sign-in').show();
+		$('#slideout').removeClass('is-visible');
+		$('#nav-screen').removeClass('is-visible');
+	});
+
+	$('#sign-in-close').click( function() {
+		$('body').css('overflow', 'scroll');
+		$('#screen').hide();
+		$('#sign-in').hide();
 	});
 	
-	// $(document).scrollTop($('#sign-up-errors').position().top-header_height-40 )
-
 
 	// Sign Up Modal
 	$('#get-started-link').click( function() {
