677f44ba7e387e9a4f46fdd21718a0f5f0d3d4c2
Michael Hickman <mhickman@unm.edu>
2014-11-20 15:44:39 -0700

    user update

--- app/controllers/custom_devise/registrations_controller.rb
+++ app/controllers/custom_devise/registrations_controller.rb
@@ -3,18 +3,16 @@ class CustomDevise::RegistrationsController < Devise::RegistrationsController
 	
 
 	def create
-
 		# super
-    build_resource(sign_up_params)
+    		build_resource(sign_up_params)
 		if resource.save
-      set_flash_message :notice, :signed_up
+     		set_flash_message :notice, :signed_up
 			@institution = Institution.new(institution_params)
 			@institution.save!
-    else
-      clean_up_passwords(resource)
-      render 'static/home', :anchor => "sign_up"
-    end
-
+   		 else
+			clean_up_passwords(resource)
+			render 'static/home', :anchor => "sign_up"
+    		end
 	end
 
 	
@@ -24,6 +22,10 @@ class CustomDevise::RegistrationsController < Devise::RegistrationsController
 
 
 private
+	def sign_up_params
+		params.require(:user).permit(:name, :phone, :email, :title, :password, :password_confirmation)
+	end
+
 	def institution_params
 		params.require(:institution).permit(:name, :code, :street, :address, :city, :zip, :phone)
 	end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141017215150) do
+ActiveRecord::Schema.define(version: 20141120201757) do
 
   create_table "institutions", force: true do |t|
     t.string   "name"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 20141017215150) do
     t.integer  "institution_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "phone"
+    t.string   "title"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
