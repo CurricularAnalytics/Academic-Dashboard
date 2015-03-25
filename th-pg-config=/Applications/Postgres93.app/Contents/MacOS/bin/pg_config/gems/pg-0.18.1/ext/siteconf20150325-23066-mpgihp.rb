c8d971f5cfdd14aafdddb6edd2695be82905054b
Michael Hickman <mhickman@unm.edu>
2015-03-25 14:46:46 -0600

    updated gems

---
+++ th-pg-config=/Applications/Postgres93.app/Contents/MacOS/bin/pg_config/gems/pg-0.18.1/ext/siteconf20150325-23066-mpgihp.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20150325-23066-1jmtt3c"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
